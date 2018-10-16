bd84d10522914349eb1a70f15ab8fdee82df29fd
mmilic1994 <33220095+mmilic1994@users.noreply.github.com>
2018-10-16 16:34:34 +0200

    I created a question table

---
+++ database/migrations/2018_10_16_142508_create_questions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user_id');
+            $table->string('title')->nullable();
+            $table->timestamp('text')->nullable();
+            $table->string('created_at')->nullable();
+            $table->string('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
