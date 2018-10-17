543b9c314c1fbcff337ab954c7317a25b3ecf1d0
mmilic1994 <33220095+mmilic1994@users.noreply.github.com>
2018-10-17 11:32:58 +0200

    question and answer apps

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -3,31 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Question;
+use App\Answer;
 class QuestionController extends Controller
 {
    public function index()
    {
-    $questions = DB::table('questions')
-        // ->where('questions_id', 1)
-        // ->orderBy('created_at', 'asc')
-        // ->get();
-        
-    dd($questions);
-
-    return 'This is the list of questions';
+    $question = Question::orderBy('created_at', 'asc')->get();
+    dd($question);
+    return 'This is a list of questions';
    }
 
    public function show()
    {
-    $question = DB::table('questions')
-        ->where('id', 1)
-        ->first();
-
-    $answers = DB::table('answers')
-        ->where('questions_id', 1)
-        ->orderBy('created_at', 'asc')
-        ->get();
+    $question =  Question::where('id', 1)->first();
+    $answers = Answer::where('questions_id', 1)->orderBy('created_at', 'asc')->get();
+    $answers = $question->answers;
     return 'This is a detail of a question';
     
    }
